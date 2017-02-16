f463b561f52292369f0233a508f39d60071a7f69
KarinaKurm <karina.kurm@gmail.com>
2017-02-16 14:56:56 -0500

    conflicts fixed

--- server/main.js
+++ server/main.js
@@ -1,14 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-<<<<<<< HEAD
-import '../imports/api/messages.js';
-import '../imports/api/rooms.js';
-=======
+
 import '../imports/lib/messages.js';
 import '../imports/lib/rooms.js';
 
+
 import { Messages } from '../imports/lib/messages.js';
 import { OCADrooms } from '../imports/lib/rooms.js';
->>>>>>> ui
+
 
 Meteor.startup(() => {
   Messages.remove({});
